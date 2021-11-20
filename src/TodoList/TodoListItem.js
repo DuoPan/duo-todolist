b6f9b5d235178103a9234adcb1ee8e61ecf96dd2
Duo <pd54007@gmail.com>
2021-11-20 15:32:44 +1100

    Use material ui to implement the styles.

--- src/TodoList/TodoListItem.js
+++ src/TodoList/TodoListItem.js
@@ -1,22 +1,109 @@
-import React from "react";
+import React, { useState } from "react";
+import Card from '@mui/material/Card';
+import Fade from '@mui/material/Fade';
+import InputBase from '@mui/material/InputBase';
+import IconButton from '@mui/material/IconButton';
+import Tooltip from '@mui/material/Tooltip';
+import TaskAltIcon from '@mui/icons-material/TaskAlt';
+import RadioButtonUncheckedIcon from '@mui/icons-material/RadioButtonUnchecked';
+import DeleteIcon from '@mui/icons-material/DeleteOutline';
+import EditIcon from '@mui/icons-material/EditOutlined';
+import SaveIcon from '@mui/icons-material/Save';
+import CancelIcon from '@mui/icons-material/Block';
+import { makeStyles } from "@material-ui/core/styles";
+
+const useStyles = makeStyles({
+  input: {
+    '& textarea': {
+      cursor: ({isEditMode}) => isEditMode ? 'text' : 'default',
+      color: ({completed}) => completed ? '#b5afaf' : 'inherit',
+      borderBottom: ({isEditMode}) => isEditMode ? '1px solid #b5afaf' : 'none',
+      paddingLeft: 4,
+    },
+    paddingLeft: 2,
+    flexGrow: 1,
+    flexWrap: 'wrap',
+  }
+});
 
 const TodoListItem = ({ 
   data,
   updateTask,
   deleteTask,
 }) => {
+  const [text, setText] = useState(data.description);
+  const [isEditMode, setIsEditMode] = useState(false);
+  const classes = useStyles({isEditMode, completed: data.completed});
 
-  // todo add throttle
-  const toggleTaskCompleted = (e) => {
-    updateTask(data.id, {completed: e.target.checked});
+  const toggleTaskCompleted = () => {
+    if (isEditMode) {
+      return;
+    }
+    updateTask(data.id, {completed: !data.completed});
+  };
+
+  const handleDeleteCancelEdit = () => {
+    if (isEditMode) {
+      setText(data.description);
+      setIsEditMode(false);
+    } else {
+      deleteTask(data.id)
+    }
+  };
+
+  const handleEditSave = () => {
+    if (isEditMode) {
+      updateTask(data.id, {description: text});
+      setIsEditMode(false);
+    } else {
+      setIsEditMode(true);
+    }
+  };
+
+  const handleOnChange = (e) => {
+    setText(e.target.value);
   };
 
   return (
-    <div style={{display: 'flex'}}>
-      <div>{data.description}</div>
-      <input type="checkbox" checked={data.completed} id="vehicle1" name="vehicle1" value="Bike" onChange={toggleTaskCompleted} />
-      <button onClick={() => deleteTask(data.id)}>Delete</button>
-    </div>
+    <Fade in={true} >
+      <Card variant="outlined"
+        style={{
+          width: '80%', marginBottom: 16, marginTop: 16, display: 'flex', padding: '16px 8px', borderLeftWidth: 4, borderLeftColor: data.completed ? '#2e7d32' : 'inherit',
+          boxShadow: '0px 2px 1px -1px rgb(0 0 0 / 10%)'
+        }}
+      >
+        <Tooltip title={data.completed ? 'Mard as Todo' : 'Mark as Done'}>
+          <IconButton onClick={toggleTaskCompleted} style={{visibility: isEditMode ? 'hidden' : 'initial'}}>
+            {data.completed ? (
+              <TaskAltIcon color={'success'} />
+            ) : (
+              <RadioButtonUncheckedIcon />
+            )}
+          </IconButton>
+        </Tooltip>
+        <InputBase
+          multiline={true}
+          readOnly={!isEditMode}
+          className={classes.input}
+          onChange={(e) => handleOnChange(e)}
+          value={text}
+        />
+        
+        <div onClick={() => updateTask(data.id, {position: 0})}>{data.position}</div>
+        
+        <Tooltip title={isEditMode ? 'Save' : 'Edit'}>
+          <IconButton color={'primary'} onClick={handleEditSave}>
+            {isEditMode ? (<SaveIcon />) : (<EditIcon />)}
+          </IconButton>
+        </Tooltip>
+
+        <Tooltip title={isEditMode ? 'Cancel Edit' : 'Delete'}>
+          <IconButton color={'error'} onClick={handleDeleteCancelEdit}>
+            {isEditMode ? (<CancelIcon />) : (<DeleteIcon />)}
+          </IconButton>
+        </Tooltip>
+      </Card>
+    </Fade>
   );
 };
 
--- src/TodoList/index.js
+++ src/TodoList/index.js
@@ -1,10 +1,19 @@
 import React, { useEffect, useState } from "react";
 import TodoListItem from "./TodoListItem";
+import Paper from '@mui/material/Paper';
+import InputBase from '@mui/material/InputBase';
+import Divider from '@mui/material/Divider';
+import IconButton from '@mui/material/IconButton';
+import LoadingButton from '@mui/lab/LoadingButton';
+import AddIcon from '@mui/icons-material/Add';
+import SearchIcon from '@mui/icons-material/Search';
+import Tooltip from '@mui/material/Tooltip';
 
 const TodoList = () => {
   const [description, setDescription] = useState('');
-  const [list, setList] = useState([]);
   const [tasks, setTasks] = useState([]);
+  const [isSearching, setIsSearching] = useState(false);
+  const [isLoading, setIsLoading] = useState(false);
 
   const updateSingleTask = (data) => {
     const results = tasks.map((item) => {
@@ -16,18 +25,6 @@ const TodoList = () => {
     setTasks(results);
   };
 
-  const loadList = () => {
-    fetch('http://localhost:8080/lists/', {
-      method: 'GET',
-      headers: {
-        'Authorization': 'Basic ' + Buffer.from('').toString('base64'),
-        'Content-Type': 'application/json',
-      }
-    })
-    .then(res => res.json())
-    .then(data => setList(data))
-  };
-
   const loadTasksByList = (listId) => {
     fetch(`http://localhost:8080/lists/${listId}/tasks`, {
       method: 'GET',
@@ -36,8 +33,11 @@ const TodoList = () => {
         'Content-Type': 'application/json',
       }
     })
-    .then(res => res.json())
-    .then(data => setTasks(data))
+      .then(res => res.json())
+      .then(data => {
+        setTasks(data);
+        setIsLoading(false);
+      })
   };
 
   const updateTask = (taskId, data) => {
@@ -49,10 +49,10 @@ const TodoList = () => {
       },
       body: JSON.stringify(data),
     })
-    .then(res => res.json())
-    .then(data => {
-      updateSingleTask(data)
-    })
+      .then(res => res.json())
+      .then(data => {
+        updateSingleTask(data)
+      })
   };
 
   const createTask = (message) => {
@@ -62,12 +62,12 @@ const TodoList = () => {
         'Authorization': 'Basic ' + Buffer.from('').toString('base64'),
         'Content-Type': 'application/json',
       },
-      body: JSON.stringify({description: message}),
+      body: JSON.stringify({ description: message }),
     })
-    .then(res => res.json())
-    .then(data => {
-      setTasks([...tasks, data])
-    })
+      .then(res => res.json())
+      .then(data => {
+        setTasks([...tasks, data])
+      })
   };
 
   const deleteTask = (taskId) => {
@@ -78,12 +78,29 @@ const TodoList = () => {
         'Content-Type': 'application/json',
       },
     })
-    .then(res => {
-      if (res.ok) {
-        const results = tasks.filter((item) => item.id !== taskId);
-        setTasks(results);
-      }
+      .then(res => {
+        if (res.ok) {
+          const results = tasks.filter((item) => item.id !== taskId);
+          setTasks(results);
+        }
+      })
+  };
+
+  const searchTask = () => {
+    setIsSearching(true);
+    fetch(`http://localhost:8080/lists/89851872-3689-41b0-b350-a449cf4e3419/tasks?filter=${description}`, {
+      method: 'GET',
+      headers: {
+        'Authorization': 'Basic ' + Buffer.from('').toString('base64'),
+        'Content-Type': 'application/json',
+      },
     })
+      .then(res => res.json())
+      .then(data => {
+        setTasks(data);
+        // force display the loading animation, otherwise the icon would looks like flashing
+        setTimeout(() => {setIsSearching(false);}, 300);
+      })
   };
 
   const handleAddTask = () => {
@@ -95,35 +112,65 @@ const TodoList = () => {
   };
 
   useEffect(() => {
+    setIsLoading(true);
+    // assume there must be 1 list exist
+    // doesn't implement list CRUD, because it is quite similar to the task CRUD
+    const loadList = () => {
+      fetch('http://localhost:8080/lists/', {
+        method: 'GET',
+        headers: {
+          'Authorization': 'Basic ' + Buffer.from('').toString('base64'),
+          'Content-Type': 'application/json',
+        }
+      })
+        .then(res => res.json())
+        .then(data => loadTasksByList(data[0]['id']));
+    };
     loadList();
   }, []);
 
   return (
-    <div style={{display: 'flex'}}>
-      <div style={{width: '50%'}}>
-        {list.map((item, index) => {
-          return (
-            <div key={`list_${index}_${item.id}`} onClick={() => loadTasksByList(item.id)}>{item.name}</div>
-          )
-        })}
-        <button>Create List</button>
+    <div style={{ display: 'flex', margin: 20, alignItems: 'center', flexDirection: 'column' }}>
+      <div style={{ display: 'flex' }}>
+        <Paper sx={{ p: '2px 4px', display: 'flex', alignItems: 'center', width: 500 }}
+        >
+          <InputBase
+            sx={{ ml: 1, flex: 1 }}
+            placeholder="Please enter new task or search keywords"
+            value={description} onChange={(e) => { setDescription(e.target.value) }}
+          />
+          <Tooltip title={'Add Task'}>
+            <IconButton sx={{ p: '10px' }} aria-label="search" color="primary" onClick={handleAddTask} >
+              <AddIcon />
+            </IconButton>
+          </Tooltip>
+
+          <Divider sx={{ height: 28, m: 0.5 }} orientation="vertical" />
+
+          <Tooltip title={'Search Task'}>
+            <LoadingButton color="primary" sx={{ p: '10px' }} aria-label="directions" onClick={searchTask} loading={isSearching}>
+              <SearchIcon />
+            </LoadingButton>
+          </Tooltip>
+
+        </Paper>
       </div>
-      <div style={{width: '50%' }}>
-        <div style={{display: 'flex'}}>
-          <input value={description} onChange={(e) => { setDescription(e.target.value) }} />
-          <button onClick={handleAddTask}>Add</button>
-        </div>
-        {tasks.map((item, index) => {
-          return (
+      {tasks.length === 0 && !isLoading && (<div style={{marginTop: 20}}>No tasks found</div>)}
+      {tasks.length === 0 && isLoading && (<div style={{marginTop: 20}}>Loading ...</div>)}
+      {tasks.map((item, index) => {
+        return (
+          <div draggable
+          key={`task_${index}_${item.id}`}
+          >
             <TodoListItem
-              key={`task_${index}_${item.id}`}
               data={item}
               updateTask={updateTask}
               deleteTask={deleteTask}
             />
-          )
-        })}
-      </div>
+          </div>
+        )
+      })}
+
     </div>
   );
 };
